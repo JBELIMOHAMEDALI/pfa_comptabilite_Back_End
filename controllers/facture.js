6397d86abccf0c2c5310ef7a6c1fabe818ca5e92
Rihem Ayadi <55142856+ayadirihem@users.noreply.github.com>
2022-07-31 21:12:16 +0200

    Update facture.js

--- controllers/facture.js
+++ controllers/facture.js
@@ -1,41 +1,60 @@
 const queryy =require("./db_query");
 
+exports.getallAccounts=(req,res)=>{
+    const sql="select * from plan_comptable";
+    queryy.sql_request(sql,null,res);
+ 
+}
+
 exports.getall=(req,res)=>{
-    const sql="select * from facture";
+    const sql="select * from headfacturevente";
     queryy.sql_request(sql,null,res);
  
 };
 //commet
 exports.insert=(req,res)=>{
-    const values=[[[req.body.numero_facture,
-                    req.body.id_client,
-                    req.body.date_de_livraison,
-                    req.body.reference,
-                    req.body.reference_article,
-                    req.body.designation,
-                    req.body.prix_unitaire_hors_taxe,
-                    req.body.quantite_demandee,
-                    req.body.stock,
-                    req.body.conditionnement,
-                    req.body.remise,
-                    req.body.prix_unitaire_net]]];
-                    console.log(req.body)
+    let lisTid = []
+    let idHead ;
+    let idDetail;
+    const values=[[[req.body.resultsType.Journal,
+                    req.body.resultsType.Serie,
+                    req.body.resultsType.Numero,
+                    req.body.resultsType.Date,
+                    req.body.resultsType.NatureF,
+                    req.body.resultsType.NExterne,
+                    req.body.resultsType.Reference,
+                    JSON.stringify(req.body.detailsType)]]];
                     //
-    const sql="insert into facture (numero_facture, id_client, date_de_livraison, reference, reference_article, designation, prix_unitaire_hors_taxe, quantite_demandee, stock, conditionnement, remise, prix_unitaire_net) values ?";
+    
+    const sql="insert into headfacturevente (`Journal`, `Serie`, `Numero`, `Date`, `NatureF`, `NExterne`, `Reference`, `Detail`) values ?;";
     
     queryy.sql_request(sql,values,res);
-    //console.log(queryy.sql_request(sql,values,res));
 }
 //
 exports.update=(req,res)=>{
-    const values=[req.body.numero_facture,req.body.id_client,req.body.date_de_livraison,req.body.reference,req.body.reference_article,req.body.designation,req.body.prix_unitaire_hors_taxe,req.body.quantite_demandee,req.body.stock,req.body.conditionnement,req.body.remise,req.body.prix_unitaire_net,req.body.id_facture];
-    const sql=" UPDATE `facture` SET numero_facture= ?, id_client =?, date_de_livraison =?, reference =?, reference_article =?, designation =?, prix_unitaire_hors_taxe =?,quantite_demandee=?, stock =?, conditionnement =?, remise =?, prix_unitaire_net =? WHERE id_facture = ? ";
+    const values=[req.body.resultsType.Journal,
+        req.body.resultsType.Serie,
+        req.body.resultsType.Numero,
+        req.body.resultsType.Date,
+        req.body.resultsType.NatureF,
+        req.body.resultsType.NExterne,
+        req.body.resultsType.Reference,
+        JSON.stringify(req.body.detailsType),
+        req.body.resultsType.id_head];
+    const sql=" UPDATE `headfacturevente` SET Journal= ?, Serie =?, Numero =?, Date =?, NatureF =?, NExterne = ?, Reference = ?,Detail= ? WHERE id_head = ? ";
     queryy.sql_request(sql,values,res);
 }
 
 exports.delete=(req,res)=>{
     const values=[req.params.id];
     console.log(values)
-    const sql="delete from facture where id_facture = ?";
+    const sql="delete from headfacturevente where id_head = ?";
+    queryy.sql_request(sql,values,res);
+}
+
+exports.getById=(req,res)=>{
+    const values=[req.params.id];
+    console.log(values)
+    const sql="select * from headfacturevente where id_head = ?";
     queryy.sql_request(sql,values,res);
 }
