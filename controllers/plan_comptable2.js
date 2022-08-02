8d9b38d8344a79c47fbf03d037e8ad4e78b459b5
youssefbenmiled <youssefbenmiled40@gmail.com>
2022-08-02 14:00:07 +0100

    new_backend v0.1

--- controllers/plan_comptable2.js
+++
@@ -1,7 +0,0 @@
-exports.update=(req,res)=>{
-    const {nom_societe,domaine_societe,id_societe}=req.body;
-
-    const values=[nom_societe,domaine_societe,id_societe];
-    const sql="UPDATE societe SET nom_societe = ?, domaine_societe = ? WHERE id_societe = ?";
-    queryy.sql_request(sql,values,res);
-}
