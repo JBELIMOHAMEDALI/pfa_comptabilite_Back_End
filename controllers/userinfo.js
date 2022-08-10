eea766952100a19e60ccb62919e15a8d9dfe7ea0
youssefbenmiled <youssefbenmiled40@gmail.com>
2022-08-10 21:02:38 +0100

    v3.2

--- controllers/userinfo.js
+++ controllers/userinfo.js
@@ -2,7 +2,6 @@ const query = require("../functions/db_query");
 
 exports.getuserinfo = (req, res) => {
     const {id_user}=req.decoded.user
-    console.log('from info',req.session);
   const sql =
     "select firstname,lastname,photo from user where id_user=?";
   query.sql_request(sql, [id_user], res);
