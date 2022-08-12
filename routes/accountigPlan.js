0068c12226b62c83bd6ee0e42991fb0ef9af71cf
youssefbenmiled <youssefbenmiled40@gmail.com>
2022-08-12 21:13:59 +0100

    excel v3.6

--- routes/accountigPlan.js
+++ routes/accountigPlan.js
@@ -3,15 +3,15 @@ const router=express.Router();
 const planComptable=require("../controllers/accountingPlan")
 const checkAuth = require('../middleware/checkToken');
 
-router.post('/import',planComptable.importFile)
+router.post('/import',checkAuth,planComptable.importFile)
 
-router.delete('/unlink/:fileName',planComptable.unlinkFile)
+router.delete('/unlink/:fileName',checkAuth,planComptable.unlinkFile)
 
-router.get('/export/:fileName',planComptable.exportFile)
+router.get('/export/:fileName',checkAuth,planComptable.exportFile)
 
-router.get('/get/:id_company/:srouceFile',planComptable.getaccountingPlanByCompany);
+router.get('/get/:id_company/:srouceFile',checkAuth,planComptable.getaccountingPlanByCompany);
 
-router.get('/get/sources/:id_company',planComptable.getAllUserSources);
+router.get('/get/sources/:id_company',checkAuth,planComptable.getAllUserSources);
 
 
 
