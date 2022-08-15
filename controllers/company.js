df6c30ae6a3fb06ebdb12f0f8ebadf658ecbbfd8
youssefbenmiled <youssefbenmiled40@gmail.com>
2022-08-15 18:27:04 +0100

    v4.2

--- controllers/company.js
+++ controllers/company.js
@@ -1,25 +1,25 @@
 const query = require("../functions/db_query");
+const dbClient = require("../config/db_config");
 
 exports.getUserCompanies = (req, res) => {
-    const {id_user}=req.decoded.user;
+  const { id_user } = req.decoded.user;
   const sql =
     "select company.* from company join user on user.id_user=company.id_user where user.id_user=?";
   query.sql_request(sql, [id_user], res);
 };
 
-exports.getfirstCompany = (req, res) => {
-  const {id_user}=req.decoded.user;
-const sql =
-  "select id_company from company join user on user.id_user=company.id_user where user.id_user=? LIMIT 1";
-query.sql_request(sql, [id_user], res);
+exports.getSelectedCompany = (req, res) => {
+  const { id_user } = req.decoded.user;
+  const sql =
+    "select id_company from company join user on user.id_user=company.id_user where user.id_user=? AND selected = ?";
+  query.sql_request(sql, [id_user, "1"], res);
 };
 
-
 exports.insert = (req, res) => {
-  const {id_user}=req.decoded.user;
+  const { id_user } = req.decoded.user;
 
   const { name, domain } = req.body;
-  const values = [[[name, domain,id_user]]];
+  const values = [[[name, domain, id_user]]];
   //
   const sql = "INSERT INTO company(name, domain,id_user) VALUES ?";
 
@@ -28,18 +28,45 @@ exports.insert = (req, res) => {
 //
 exports.update = (req, res) => {
   const { name, domain, id_company } = req.body;
-  const {id_user}=req.decoded.user;
+  const { id_user } = req.decoded.user;
 
-  const values = [name, domain, id_company,id_user];
+  const values = [name, domain, id_company, id_user];
   const sql =
     "UPDATE company SET name = ?, domain = ? WHERE id_company = ? and id_user =?";
   query.sql_request(sql, values, res);
 };
 
 exports.delete = (req, res) => {
-  const {id_user}=req.decoded.user;
+  const { id_user } = req.decoded.user;
 
-  const values = [req.params.id,id_user];
+  const values = [req.params.id, id_user];
   const sql = "delete from company where id_company = ? and id_user=?";
   query.sql_request(sql, values, res);
+};
+
+exports.setSelected = (req, res) => {
+  const { id_company } = req.params;
+  const { id_user } = req.decoded.user;
+  let sql =
+    "UPDATE company SET selected=? WHERE id_company <> ? and id_user =? and selected=?";
+
+  dbClient.query(sql, ["0", id_company, id_user, "1"], (err, rows) => {
+    if (!err) {
+      sql = "UPDATE company SET selected=? WHERE id_company = ? and id_user =?";
+
+      query.sql_request(sql, ["1", id_company, id_user], res);
+    } else
+      return res.status(500).json({
+        err: true,
+        message: "Operation not performed! Try again later",
+      });
+  });
+};
+
+
+exports.verifySelectedCompany = (req, res) => {
+  const { id_user } = req.decoded.user;
+  const sql =
+    "select id_company from company join user on user.id_user=company.id_user where user.id_user=? AND selected = ?";
+  query.sql_request(sql, [id_user, "1"], res);
 };
--- routes/company.js
+++ routes/company.js
@@ -4,11 +4,14 @@ const company=require("../controllers/company");
 const checkToken = require('../middleware/checkToken');
 
 router.get("/get",checkToken,company.getUserCompanies);
-router.get("/get/first",checkToken,company.getfirstCompany);
+router.get("/get/selected",checkToken,company.getSelectedCompany);
+router.get("/verify/selected",checkToken,company.verifySelectedCompany);
 
 router.post("/add",checkToken,company.insert);
 router.put("/update",checkToken,company.update);
 router.delete("/delete/:id",checkToken,company.delete);
+router.put("/set/selection/:id_company",checkToken,company.setSelected);
+
 
 module.exports=router;
  
