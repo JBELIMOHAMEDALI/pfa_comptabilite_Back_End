3cdf3148c6a53b23fe49b40d07d9a7e2bf6c4251
youssefbenmiled <youssefbenmiled40@gmail.com>
2022-08-18 04:44:53 +0100

    v4.9

--- controllers/accountingPlan.js
+++ controllers/accountingPlan.js
@@ -9,8 +9,7 @@ const storage = multer.diskStorage({
     callback(null, "./uploads/excel-files"); //filename already used
   },
   filename: (req, file, callback) => {
-    const filename = file && file.originalname.split(".")[0];
-    callback(null, filename + ".xls");
+    callback(null, `${Date.now()}.xlsx`);
   },
 });
 
@@ -56,28 +55,27 @@ module.exports.importFile = (req, res) => {
 function importFileToDb(req, res) {
   const { id_company } = req.params;
   readXlsxFile(req.file.path)
-    .then((rows) => {
-      const filename = req.file.filename;
-      rows.map((row) => {
-        row.push(filename);
-        row.push(id_company);
-      });
-      const sql =
-        "INSERT INTO accounting_plan (`account_number`,`description`,`source`,`id_company`) VALUES ?";
-      dbClient.query(sql, [rows], (err, rows) => {
+    .then((fileRows) => {
+      const { filename, originalname } = req.file;
+
+      let sql =
+        "INSERT INTO accounting_plan_source (`source`,`upload`) VALUES ?";
+      dbClient.query(sql, [[[originalname, filename]]], (err, rows) => {
         if (!err) {
-          return res.status(200).json({
-            err: false,
-            message: "File imported succesfully !",
+          fileRows.map((row) => {
+            row.push(rows.insertId);
+            row.push(id_company);
           });
-        } else
+          sql =
+            "INSERT INTO accounting_plan (`account_number`,`description`,`id_source`,`id_company`) VALUES ?";
+          query.sql_request(sql, [fileRows], res);
+        } else {
           return res.status(500).json({
             err: true,
-            message: "File must contains 2 columns ! ",
+            message: "Operation not performed ! Try again later",
           });
+        }
       });
-
-      // query.sql_request(sql, [rows], res);
     })
     .catch((error) => {
       return res.status(500).json({
@@ -86,19 +84,17 @@ function importFileToDb(req, res) {
       });
     });
 }
-
 module.exports.unlinkFile = (req, res) => {
-  const { filename, id_company } = req.params;
-
-  fs.unlink(`${process.env.FILES_PATH}/${filename}`, (err) => {
-    if (err) {
-      return res.status(500).json({
-        err: true,
-        message: err.message,
-      });
-    }
-    const sql = `delete from accounting_plan where source = ? and id_company = ?`;
-    query.sql_request(sql, [filename, id_company], res);
+  const { uploadFile } = req.params;
+  fs.unlink(`${process.env.FILES_PATH}/${uploadFile}`, (err) => {
+    // if (err) {
+    //   return res.status(500).json({
+    //     err: true,
+    //     message: err.message,
+    //   });
+    // }
+    const sql = `delete from accounting_plan_source where accounting_plan_source.upload = ?`;
+    query.sql_request(sql, [uploadFile], res);
   });
 };
 
@@ -141,39 +137,44 @@ module.exports.unlinkFile = (req, res) => {
 // };
 
 module.exports.getaccountingPlanByCompany = (req, res) => {
-  const { id_company, sourceFile } = req.params;
+  const { id_company, uploadFile } = req.params;
   const { limit, offset } = req.query;
   let sql;
   if (req.query.limit) {
     sql = `select accounting_plan.id , accounting_plan.account_number , accounting_plan.description, 
     accounting_plan.id_company , (SELECT COUNT(*) FROM accounting_plan 
-    join company on company.id_company=accounting_plan.id_company 
-    where accounting_plan.id_company=? and accounting_plan.source =?) 
+    join accounting_plan_source
+    on accounting_plan_source.id=accounting_plan.id_source AND                      
+    accounting_plan_source.upload = ?) 
     AS totalItems  
-    from accounting_plan
-    join company on company.id_company=accounting_plan.id_company 
-     where accounting_plan.id_company=? and accounting_plan.source =?
-     LIMIT ${limit} OFFSET ${offset}`;
+    from accounting_plan join accounting_plan_source
+    join company on company.id_company=accounting_plan.id_company and         
+    accounting_plan_source.id=accounting_plan.id_source
+     where accounting_plan.id_company=? and accounting_plan_source.upload =?
+     LIMIT ${limit} OFFSET  ${offset}`;
   } else {
     //export case
     sql = `select  accounting_plan.account_number , accounting_plan.description     
-    from accounting_plan
-    join company on company.id_company=accounting_plan.id_company 
-     where accounting_plan.id_company=? and accounting_plan.source =? `;
+    from accounting_plan join accounting_plan_source
+    join company on company.id_company=accounting_plan.id_company and
+    accounting_plan_source.id=accounting_plan.id_source
+     where accounting_plan.id_company=? and accounting_plan_source.upload =? `;
   }
 
   query.sql_request(
     sql,
-    [id_company, sourceFile, id_company, sourceFile],
+    req.query.limit
+      ? [uploadFile, id_company, uploadFile]
+      : [id_company, uploadFile],
     res,
     true
   );
 };
 
 module.exports.getAllSources = (req, res) => {
-  const sql = `select distinct source from accounting_plan 
-join company on company.id_company=accounting_plan.id_company 
-where accounting_plan.id_company = ? `;
+  const sql = `select accounting_plan_source.* from accounting_plan_source 
+join company join accounting_plan on company.id_company=accounting_plan.id_company 
+where accounting_plan.id_company = ? GROUP BY id`;
   query.sql_request(sql, [req.params.id_company], res);
 };
 
@@ -186,15 +187,26 @@ module.exports.deleterow = (req, res) => {
 
 module.exports.updaterow = (req, res) => {
   const { id_row } = req.params;
-  const { col, description } = req.body;
+  const { account_number, description } = req.body;
 
-  const sql = `UPDATE accounting_plan SET account_number=?,description=? where id=? `;
+  const sql = `UPDATE accounting_plan SET account_number=?,description=?   
+  where accounting_plan.id=? `;
   query.sql_request(sql, [account_number, description, id_row], res);
 };
 
 module.exports.addrow = (req, res) => {
-  const { account_number, description, source, id_company } = req.body;
+  const { account_number, description, id_source, id_company } = req.body;
   const sql =
-    "INSERT INTO accounting_plan (`account_number`,`description`,`source`,`id_company`) VALUES ?";
-  query.sql_request(sql, [[[account_number, description, source, id_company]]], res);
+    "INSERT INTO accounting_plan (`account_number`,`description`,`id_source`,`id_company`) VALUES ?";
+  query.sql_request(
+    sql,
+    [[[account_number, description, id_source, id_company]]],
+    res
+  );
+};
+
+module.exports.addAccountingPlan = (req, res) => {
+  const { source } = req.body;
+  const sql = "INSERT INTO accounting_plan_source (`source`,`upload`) VALUES ?";
+  query.sql_request(sql, [[[source, `${Date.now()}.xlsx`]]], res);
 };
--- routes/accountingPlan.js
+++ routes/accountingPlan.js
@@ -4,15 +4,17 @@ const accountingPlan=require("../controllers/accountingPlan")
 const checkAuth = require('../middleware/checkToken');
 
 router.post('/import/:id_company',checkAuth,accountingPlan.importFile);
-router.delete('/unlink/:id_company/:filename',checkAuth,accountingPlan.unlinkFile);
+router.delete('/unlink/:id_company/:uploadFile',checkAuth,accountingPlan.unlinkFile);
 // router.get('/export/:filename',checkAuth,accountingPlan.exportFile);
 router.get('/get/sources/:id_company',checkAuth,accountingPlan.getAllSources);
-router.get('/get/:id_company/:sourceFile',checkAuth,accountingPlan.getaccountingPlanByCompany);
+router.get('/get/:id_company/:uploadFile',checkAuth,accountingPlan.getaccountingPlanByCompany);
 
 //CRUD
 router.put('/update/:id_row',checkAuth,accountingPlan.updaterow);
-router.post('/add',checkAuth,accountingPlan.addrow);
+router.post('/add/row',checkAuth,accountingPlan.addrow);
 router.delete('/delete/row/:id_row',checkAuth,accountingPlan.deleterow);
+
+router.post('/add',checkAuth,accountingPlan.addAccountingPlan);
 
 
 module.exports=router;
