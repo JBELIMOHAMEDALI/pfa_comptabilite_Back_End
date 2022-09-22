d717ed42d444cb72ddeb2c22416dafb20056a1b4
JBELIMOHAMEDALI <mohem12ali@gmail.com>
2022-09-22 18:01:52 +0100

    6.1

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,9 +1,13 @@
 const queryy =require("../functions/db_query");
 
 
+// 
+exports.get = (req, res) => {
 
-exports.get=(req,res)=>{
-    return res.json({
-        stats:true
-    })
-};
+     let  sql = `SELECT SUM(product.cost)+(SELECT SUM(service.sale_price+(service.sale_price*service.tax))
+      from service WHERE service.id_company = ${req.params.id_company} and service.operation= ${req.params.operation} ) 
+      AS totalprix from product WHERE
+       product.id_company = ${req.params.id_company} and product.operation=${req.params.operation}`;
+
+    queryy.sql_request(sql, null, res);
+  };
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,7 @@ const dashboard=require("../controllers/dashboard");
 const checkToken=require("../middleware/checkToken");
 
 
-router.get("/get",checkToken,dashboard.get);
+router.get("/get/:id_company/:operation",checkToken,dashboard.get);
 
 module.exports=router;
  
